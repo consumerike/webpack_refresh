55cb47a1f12460383733d3bd371c1f5869ba2077
Ikechi <consumerike@gmail.com>
2017-04-30 08:39:13 -0500

    added babel loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,25 @@
-module.exports= {
+module.exports = {
 
-    //define entry point
-    entry: './src/script1.js',
+  //define entry point
+  entry: './src/script1.js',
 
-    //define outpoint point
-    output: {
-        path: '/dist/bundle.js',
-        filename: 'bundle.js'
-    }
+  //define outpoint point
+  output: {
+    path: '/dist/bundle.js',
+    filename: 'bundle.js'
+  },   
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules/),
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
 };
 
